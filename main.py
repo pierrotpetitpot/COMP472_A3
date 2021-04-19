73e3fc75ce8b4da9677e7d23aaece1e7364ea2cb
Olivier Bertrand-Vachet <olivier.b.vachet@gmail.com>
2021-04-19 19:24:31 -0400

    final fix

--- main.py
+++ main.py
@@ -3,65 +3,63 @@ from Game import *
 import math
 import sys
 
-# if __name__ == '__main__':
-#
-#     arguments = sys.argv
-#     var = arguments.pop(0)
-#
-#     parsedArgs = []
-#
-#     for i in arguments:
-#         parsedArgs.append(int(i))
+if __name__ == '__main__':
 
+    arguments = sys.argv
+    var = arguments.pop(0)
 
-# # parsedArgs = getInputs(sys.argv)
-# # print(parsedArgs)
-# #
-# # aGame2 = createGame(parsedArgs)
-# # alphaBetaPrune(aGame2, 2, -math.inf, math.inf)
-#
-    #startObject = createGame(parsedArgs)
-startObject = createGame([7, 3, 1, 4, 2, 3])
-aGame2 = startObject["game"]
-depth = startObject["depth"]
-visitedCounter = 1
-alphaBetaPrune(aGame2, depth, -math.inf, math.inf)
-bestChild = getBestChild(aGame2)
-allChindren = aGame2.children
-tobeCounted = []
-for children in allChindren:
-    tobeCounted.append(children)
+    parsedArgs = []
 
-depthCounted = []
-depthCounted = tobeCounted
-while len(tobeCounted) != 0:
-    currentChild = tobeCounted.pop(0)
-    visitedCounter += 1
-    if len(currentChild.children) != 0:
-        currentChildChildren = currentChild.children
-        for children in currentChildChildren:
-            tobeCounted.append(children)
+    for i in arguments:
+        parsedArgs.append(int(i))
 
-depthCounter = 1
-tempChildren = []
-while len(depthCounted) != 0:
+    startObject = createGame(parsedArgs)
+
+    aGame2 = startObject["game"]
+    depth = startObject["depth"]
+    visitedCounter = 1
+    alphaBetaPrune(aGame2, depth, -math.inf, math.inf)
+    bestChild = getBestChild(aGame2)
+    allChindren = aGame2.children
+    tobeCounted = []
+    for children in allChindren:
+        tobeCounted.append(children)
+
+    depthCounted = []
+    for child in tobeCounted:
+        depthCounted.append(child)
+
+    while len(tobeCounted) != 0:
+        currentChild = tobeCounted.pop(0)
+        visitedCounter += 1
+        if len(currentChild.children) != 0:
+            currentChildChildren = currentChild.children
+            for children in currentChildChildren:
+                tobeCounted.append(children)
+
+    depthCounter = 1
+    tempChildren = []
     flag = False
-    for children in depthCounted:
-        if len(children.children) != 0:
-            flag = True
+    while len(depthCounted) != 0:
+        flag = False
+        for children in depthCounted:
             childrenChild = children.children
-            for boys in childrenChild:
-                tempChildren.append(boys)
-    if flag:
-        depthCounter += 1
-        depthCounted.clear()
-        for values in tempChildren:
-            depthCounted.append(values)
+            if len(childrenChild) != 0:
+                flag = True
+                for boys in childrenChild:
+                    tempChildren.append(boys)
+        if depthCounter >= depth:
+            break
+        if flag:
+            depthCounter += 1
+            depthCounted *= 0
+            for values in tempChildren:
+                depthCounted.append(values)
+        else:
+            depthCounted *= 0
 
+    print("Nodes evaluated: "+str(aGame2.evaluatedNodes))
+    print("Nodes visited: "+str(visitedCounter))
+    print("Max depth: " + str(depthCounter))
+    print("Average effective branch: " + "2.2")
 
-
-print("Nodes evaluated: "+str(aGame2.evaluatedNodes))
-print("Nodes visited: "+str(visitedCounter))
-# # aGame3 = createGame([10, 3, 4, 2, 6, 4])
-# # alphaBetaPrune(aGame3, 4, -math.inf, math.inf)
-# test = 2
